5d391abf339fe2cad4ca363985d44d4518efe779
Saiful-nstu <saifulnstuice@gmail.com>
2020-05-30 01:13:24 +0600

    score Add

--- game.js
+++ game.js
@@ -22,8 +22,11 @@ window.onload = function () {
     startTimer(fiveMinutes, display);
 };
 
+/*question part */
 const question =document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptAnswers = false;
@@ -132,6 +135,7 @@ getNewQuestion = () =>{
         return window.location.assign('end.html')
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter} / ${MAX_QUESTIONS}`;
     const currentIndex= Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[currentIndex];
     question.innerText = currentQuestion.question;
@@ -154,7 +158,7 @@ choices.forEach(choice =>{
         const classToApply = selectedAnswer == currentQuestion.answer? 'correct':'incorrect';
      //   selectedAnswer == currentQuestion.answer? alert("Good Job Baby!!!",value):alert("Gorer Dim parse!!!!")
     
-
+        if(classToApply == 'correct') incrementScore(CORRECT_BONUS)
         choice.parentElement.classList.add(classToApply);
         
         setTimeout(()=>{
@@ -168,4 +172,8 @@ choices.forEach(choice =>{
     });
     
 });
+incrementScore = num =>{
+    score += num;
+    scoreText.innerText = score;
+}
 startGame();
